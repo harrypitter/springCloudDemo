55a253dbca1d649e4832e1cca196a34d12249b7c
lidu <li.du@taosecret.com>
2019-03-02 22:07:25 +0800

    增加聚合仪表盘HystrixTurbine

---
+++ HystrixTurbine/src/main/java/com/cloud/hystrixturbine/HystrixTurbineApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.hystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
+public class HystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixTurbineApplication.class, args);
+	}
+
+}
